94cd9b7c4bc8f3e9922b545635532294045d6245
unknown <kathanmajithia@gmail.com>
2025-08-27 12:06:46 +0530

    Upper part complete

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import back from "../assets/back.png";
+import image from "../assets/pothole1.png";
 
 const Report = () => {
   useEffect(() => {
@@ -9,6 +10,13 @@ const Report = () => {
       document.body.style.overflow = "auto";
     };
   }, []);
+  var by = "Arjun Patel";
+  var da = "5th Sept 2025";
+  var ti = "2:00 pm";
+  var loc = "Anand,Gujarat";
+  var dep = "Anand Municipal Corporation";
+  var des =
+    "There is a large and deep pothole on the main road in Bakrol, near. It is located right in the middle of the road and is a significant hazard for two-wheelers and other vehicles. The recent rains have made it difficult to spot, increasing the risk of accidents. ";
   let problems = [
     "Pothole",
     "Dumping",
@@ -20,7 +28,7 @@ const Report = () => {
     "Traffic Signal Issue",
     "Others",
   ];
-  let image = "../assets/pothole1.png";
+  //   let image = "../assets/pothole1.png";
   return (
     <>
       <div
@@ -44,17 +52,57 @@ const Report = () => {
         <div>
           <div>
             <div className="text-4xl font-bold">Report of Pothole</div>
-            <div className="flex gap-6">
-              <div className="w-1/2">Img</div>
-              <div className="flex w-1/2 flex-col justify-start gap-5 space-y-4">
-                <div>Details</div>
-                <div>Reported By</div>
-                <div>Date/Time</div>
-                <div>Location</div>
-                <div>Department</div>
+            <div className="flex">
+              <div className="mt-5 w-1/3 rounded-xs">
+                <img className="h-100" src={image} alt="IMG" />
+              </div>
+              <div className="flex w-2/3 flex-col justify-start gap-4 space-y-3">
+                <div className="mt-5 text-3xl font-bold">Details</div>
+                <div className="text-xl font-bold">Reported By : {by}</div>
+                <div className="text-xl font-bold">
+                  Date/Time :{" "}
+                  <div className="inline font-medium">
+                    {da} | {ti}
+                  </div>
+                </div>
+                <div className="text-xl font-bold">
+                  Location
+                  <div className="inline font-medium">
+                    :{" "}
+                    <a href="#" className="text-blue-700 underline">
+                      {loc}
+                    </a>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="inline h-6 w-6 text-blue-500"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                      />
+                    </svg>
+                    <span className="text-sm">Click to open in map</span>
+                  </div>
+                </div>
+                <div className="text-xl font-bold">
+                  Department
+                  <div className="inline font-medium">: {dep}</div>
+                </div>
                 <div>
-                  <div>Description</div>
-                  <div>Detail</div>
+                  <div className="text-xl font-bold">
+                    Description
+                    <div className="mr-10 text-justify font-medium">{des}</div>
+                  </div>
                 </div>
               </div>
             </div>
