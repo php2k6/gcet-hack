12e8f3de7a62a8b7e559e6443caa2423cf0a84dc
Nisarg-Panchal876 <2023002257.gcet@cvmu.edu.in>
2025-08-29 19:10:39 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine
 from app.models import Base
-from app.routers import chatbot, auth, users, issues, authorities, votes, stats
+from app.routers import chatbot, auth, users, issues, authorities, votes, stats, heatmap
 from fastapi.middleware.cors import CORSMiddleware
 
 # Import all models to ensure they're registered
